ae83d2f932769ebb3c569d55174014614f24a309
ScignScape <axiatropicsemantics@gmail.com>
2026-09-01 13:16:19 -0400

    update

--- code/cpp/src/prelim/contrib/all/all/dgk/gtagml/gtagml/gtagml/kernel/grammar/gtagml-grammar.cpp
+++ code/cpp/src/prelim/contrib/all/all/dgk/gtagml/gtagml/gtagml/kernel/grammar/gtagml-grammar.cpp
@@ -140,7 +140,7 @@ void GTagML_Grammar::init(GTagML_Parser& p, GTagML_Graph& g, GTagML_Parse_State&
 
  add_rule( gtagml_context, "tag-command-entry-no-supl",
    " (?<blank-lines> .blank-lines.?) "
-   " (?<outer> [[{(] )  (?<pre> [^\\s\\[\\]`]*) ` "
+   " (?<outer> [[{(] )  (?<pre> [^\\s\\[\\]}`]*) ` "
    " (?<main> (?: [^\\s\\[\\]`,;.] | (?: \\s+ ->> \\s+ ) )+ ) "
    " (?<post> [,;.]+ )"
    ,[&] //raw_context, &parse_state, this, &p]
