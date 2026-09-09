38b2ad7560ec5c60f3c7edb8837909a9a810544b
ScignScape <axiatropicsemantics@gmail.com>
2026-09-08 21:52:25 -0400

    update

--- code/cpp/src/prelim/contrib/all/all/sk/dogpack/dogpal/dogpal-vm/modules/asl-module.cxx
+++ code/cpp/src/prelim/contrib/all/all/sk/dogpack/dogpal/dogpal-vm/modules/asl-module.cxx
@@ -7,15 +7,15 @@
 
 
 #if METHODS_String
-   {"sdi-sentence-end-punctuation", (methods_String) &SDI_Module::sentence_end_punctuation},
-   {"sdi-sentence-text", (methods_String) &SDI_Module::sentence_text},
+   {"asl-sentence-end-punctuation", (methods_String) &SDI_Module::sentence_end_punctuation},
+   {"asl-sentence-text", (methods_String) &SDI_Module::sentence_text},
 #elif METHODS_x0
 #elif METHODS_U4x1
-   {"sdi-new-sentence", (methods_U4x1) &SDI_Module::new_sentence},
+   {"asl-new-sentence", (methods_U4x1) &SDI_Module::new_sentence},
 #elif METHODS_U4x4
-   {"sdi-sentence-end-pos", (methods_U4x4) &SDI_Module::sentence_end_pos},
-   {"sdi-sentence--end-pos", (methods_U4x4) &SDI_Module::sentence__end_pos},
-   {"sdi-sentence-switch-pos", (methods_U4x4) &SDI_Module::sentence_switch_pos},
+   {"asl-sentence-end-pos", (methods_U4x4) &SDI_Module::sentence_end_pos},
+   {"asl-sentence--end-pos", (methods_U4x4) &SDI_Module::sentence__end_pos},
+   {"asl-sentence-switch-pos", (methods_U4x4) &SDI_Module::sentence_switch_pos},
 
 #endif
 
